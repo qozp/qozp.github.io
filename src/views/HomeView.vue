67eaa31aa458ea832970908f9a45681f28353ad5
CarelessWisp <isaiahpham04@gmail.com>
2025-08-28 14:11:42 -0400

    Tailwind working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@
   <div
     class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white"
   >
-    <h1 class="text-5xl font-bold mb-4">🚀 Tailwind is working!</h1>
-    <p class="text-lg">If you see gradient background + styled text, Tailwind is set up.</p>
+    <h1 class="text-5xl font-bold mb-4">Tailwind is working</h1>
   </div>
 </template>
