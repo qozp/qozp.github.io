a096a01c82a517065939a8e7ee2913a6f329989d
Qozp <isbpham@gmail.com>
2025-09-22 16:27:41 -0400

    add 404 html test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/components/pages/HomePage.vue'
 import CountdownPage from '@/components/pages/CountdownPage.vue'
 import SlotsPage from '@/components/pages/SlotsPage.vue'
@@ -6,11 +6,11 @@ import SlotsPage from '@/components/pages/SlotsPage.vue'
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
     { path: '/countdown', name: 'Countdown', component: CountdownPage },
-    { path: '/slots', component: SlotsPage },
+    { path: '/slots', name: 'Slots', component: SlotsPage },
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 })
 
