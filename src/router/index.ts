82ce21ad29c365480a28d0c3c4e4a923066ee88d
Qozp <isbpham@gmail.com>
2025-10-20 15:40:01 -0400

    Remove countdown page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
-import CountdownPage from '@/pages/CountdownPage.vue'
+// import CountdownPage from '@/pages/CountdownPage.vue'
 import SlotsPage from '@/pages/SlotsPage.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
-    { path: '/countdown', name: 'Countdown', component: CountdownPage },
+    // { path: '/countdown', name: 'Countdown', component: CountdownPage },
     { path: '/slots', name: 'Slots', component: SlotsPage },
 ]
 
